441289addf92cf7d70c5f7be001e96c53d7f8f0a
Harigopal Udayakumar (Consultant) <udayakumar.harigopal@consultant.volvo.com>
2022-03-01 21:08:20 +0100

    updated and working

--- app/src/main/java/com/example/trial_1/NewsLinks.java
+++ app/src/main/java/com/example/trial_1/NewsLinks.java
@@ -5,14 +5,14 @@ import java.util.HashMap;
 public class NewsLinks{
 
        public HashMap<Integer, String> hm = new HashMap<Integer, String>();{
-        hm.put(1, "http://newsonair.com/Top_rss.aspx");
-        hm.put(2, "http://newsonair.com/National_rss.aspx");
+        hm.put(1, "https://newsonair.gov.in/Top_rss.aspx");
+        hm.put(2, "https://newsonair.gov.in/National_rss.aspx");
         //hm.put(2, "http://newsonair.nic.in/National_rss.aspx");
        // hm.put(2, "https://www.google.com/");
-        hm.put(3, "http://newsonair.com/State_rss.aspx");
-        hm.put(4, "http://newsonair.com/International_rss.aspx");
-        hm.put(5, "http://newsonair.com/business_rss.aspx");
-        hm.put(6, "http://newsonair.com/Sports_rss.aspx");
+        hm.put(3, "https://newsonair.gov.in/State_rss.aspx");
+        hm.put(4, "https://newsonair.gov.in/International_rss.aspx");
+        hm.put(5, "https://newsonair.gov.in/business_rss.aspx");
+        hm.put(6, "https://newsonair.gov.in/Sports_rss.aspx");
 
 
     }
